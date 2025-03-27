5d4fcc8c28c71d70ed7c967c3b55a991d77fe1a1
Camiel vs <93798047+camielvs@users.noreply.github.com>
2025-03-27 11:07:30 -0700

    Allow configuration of footer links via environment variables (#117)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,34 @@
-export const ABOUT_URL = "https://cloud-pipelines.net/";
-export const GIVE_FEEDBACK_URL =
-  "https://github.com/Cloud-Pipelines/pipeline-editor/issues";
-export const PRIVACY_POLICY_URL = "https://cloud-pipelines.net/privacy_policy";
-export const API_URL = import.meta.env.VITE_BACKEND_API_URL ?? "";
+/* Environment Config */
+export const ABOUT_URL = getEnvWithFallback(
+  "VITE_ABOUT_URL",
+  "https://cloud-pipelines.net/",
+);
 
+export const GIVE_FEEDBACK_URL = getEnvWithFallback(
+  "VITE_GIVE_FEEDBACK_URL",
+  "https://github.com/Cloud-Pipelines/pipeline-editor/issues",
+);
+
+export const PRIVACY_POLICY_URL = getEnvWithFallback(
+  "VITE_PRIVACY_POLICY_URL",
+  "https://cloud-pipelines.net/privacy_policy",
+);
+
+export const API_URL = getEnvWithFallback("VITE_BACKEND_API_URL", "");
+
+export const GIT_REPO_URL = getEnvWithFallback(
+  "VITE_GIT_REPO_URL",
+  "https://github.com/Cloud-Pipelines/pipeline-studio-app",
+);
+
+export const GIT_COMMIT = getEnvWithFallback("VITE_GIT_COMMIT", "master");
+
+function getEnvWithFallback(key: string, fallback: string): string {
+  const value = import.meta.env[key];
+  return value && value.length ? value : fallback;
+}
+
+/* App Config */
 export const EDITOR_PATH = "/editor";
 export const RUNS_BASE_PATH = "/runs";
 export const APP_ROUTES = {
