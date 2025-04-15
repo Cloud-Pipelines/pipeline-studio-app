5c9d2eed4a170af6771f95de15cd16f0c873890c
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-04-15 10:34:23 -0700

    Migrate componentspec loader into Provider

--- src/DragNDrop/DnDFlow.tsx
+++ src/DragNDrop/DnDFlow.tsx
@@ -16,16 +16,14 @@ import {
   MiniMap,
   ReactFlowProvider,
 } from "@xyflow/react";
-import { useEffect, useState } from "react";
-
-import loadPipelineByName from "@/utils/loadPipelineByName";
-import { prepareComponentRefForEditor } from "@/utils/prepareComponentRefForEditor";
 
 import { getAppSettings } from "../appSettings";
 import { downloadDataWithCache } from "../cacheUtils";
-import type { ComponentSpec } from "../componentSpec";
-import { type ComponentReferenceWithSpec } from "../componentStore";
 import { PipelineAutoSaver } from "../utils/PipelineAutoSaver";
+import {
+  ComponentSpecProvider,
+  useComponentSpec,
+} from "./ComponentSpecProvider";
 import GraphComponentSpecFlow from "./GraphComponentSpecFlow";
 import Sidebar from "./Sidebar";
 
@@ -33,70 +31,86 @@ const GRID_SIZE = 10;
 
 const DnDFlow = () => {
   const location = useLocation();
-  const [isDirty, setIsDirty] = useState(false);
-  const [componentSpec, setComponentSpec] = useState<
-    ComponentSpec | undefined
-  >();
-  const [appSettings] = useState(getAppSettings());
-
-  const downloadData = downloadDataWithCache;
-
-  useEffect(() => {
-    const loadPipeline = async () => {
-      const experimentName = location.pathname.split("/").pop();
-      if (!experimentName) {
-        return;
-      }
-
-      const result = await loadPipelineByName(experimentName);
-
-      const preparedComponentRef = await prepareComponentRefForEditor(
-        result.experiment?.componentRef as ComponentReferenceWithSpec,
-      );
-
-      setComponentSpec(preparedComponentRef);
-    };
-    loadPipeline();
-  }, []);
-
-  if (componentSpec === undefined) {
-    return <></>;
-  }
-
-  const handleSetComponentSpec = (componentSpec: ComponentSpec) => {
-    setComponentSpec(componentSpec);
-    setIsDirty(true);
-  };
+  const experimentName = location.pathname.split("/").pop() || "";
 
   return (
-    <div className="dndflow">
-      <DndContext>
-        <ReactFlowProvider>
-          <div className="reactflow-wrapper">
-            <GraphComponentSpecFlow
-              componentSpec={componentSpec}
-              setComponentSpec={handleSetComponentSpec}
-              snapToGrid={true}
-              snapGrid={[GRID_SIZE, GRID_SIZE]}
-            >
-              <MiniMap />
-              <Controls />
-              <Background gap={GRID_SIZE} />
-            </GraphComponentSpecFlow>
-          </div>
-          <Sidebar
-            componentSpec={componentSpec}
-            setComponentSpec={handleSetComponentSpec}
-            isDirty={isDirty}
-            setIsDirty={setIsDirty}
-            appSettings={appSettings}
-            downloadData={downloadData}
-          />
-          <PipelineAutoSaver componentSpec={componentSpec} />
-        </ReactFlowProvider>
-      </DndContext>
-    </div>
+    <ComponentSpecProvider experimentName={experimentName}>
+      <div className="dndflow">
+        <DndContext>
+          <ReactFlowProvider>
+            <PageWrapper />
+          </ReactFlowProvider>
+        </DndContext>
+      </div>
+    </ComponentSpecProvider>
   );
+};
+
+const PageWrapper = () => {
+  const { componentSpec } = useComponentSpec();
+
+  if (!componentSpec) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="reactflow-wrapper">
+        <GraphComponentSpecFlowWrapper />
+      </div>
+      <SidebarWrapper />
+      <PipelineAutoSaverWrapper />
+    </>
+  );
+};
+
+const GraphComponentSpecFlowWrapper = () => {
+  const { componentSpec, setComponentSpec } = useComponentSpec();
+
+  if (!componentSpec) {
+    return null;
+  }
+
+  return (
+    <GraphComponentSpecFlow
+      componentSpec={componentSpec}
+      setComponentSpec={setComponentSpec}
+      snapToGrid={true}
+      snapGrid={[GRID_SIZE, GRID_SIZE]}
+    >
+      <MiniMap />
+      <Controls />
+      <Background gap={GRID_SIZE} />
+    </GraphComponentSpecFlow>
+  );
+};
+
+const SidebarWrapper = () => {
+  const { componentSpec, setComponentSpec, isDirty, setIsDirty } =
+    useComponentSpec();
+  const appSettings = getAppSettings();
+  const downloadData = downloadDataWithCache;
+
+  return (
+    <Sidebar
+      componentSpec={componentSpec}
+      setComponentSpec={setComponentSpec}
+      isDirty={isDirty}
+      setIsDirty={setIsDirty}
+      appSettings={appSettings}
+      downloadData={downloadData}
+    />
+  );
+};
+
+const PipelineAutoSaverWrapper = () => {
+  const { componentSpec } = useComponentSpec();
+
+  if (!componentSpec) {
+    return null;
+  }
+
+  return <PipelineAutoSaver componentSpec={componentSpec} />;
 };
 
 export default DnDFlow;
