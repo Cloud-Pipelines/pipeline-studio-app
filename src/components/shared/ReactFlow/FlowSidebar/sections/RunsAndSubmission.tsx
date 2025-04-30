71d3618182ed55af8d8165b5c7f5260464c8fa63
Matt Beaulne <matt.beaulne@shopify.com>
2025-04-30 09:42:15 -0400

    Slight rework of the side menu and update view run (#218)

--- src/components/shared/ReactFlow/FlowSidebar/sections/RunsAndSubmission.tsx
+++ src/components/shared/ReactFlow/FlowSidebar/sections/RunsAndSubmission.tsx
@@ -1,13 +1,7 @@
-import { ChevronDown } from "lucide-react";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 import OasisSubmitter from "@/components/shared/OasisSubmitter";
 import RunOverview from "@/components/shared/RunOverview";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   SidebarGroup,
@@ -22,15 +16,10 @@ import { useComponentSpec } from "@/providers/ComponentSpecProvider";
 
 const RunsAndSubmission = () => {
   const { componentSpec } = useComponentSpec();
-  const [runsIsOpen, setRunsIsOpen] = useState(false);
 
   const { pipelineRuns, refetch } = useLoadPipelineRuns(
     componentSpec.name || "",
   );
-
-  const handleRunsCollapsedChange = (e: boolean) => {
-    setRunsIsOpen(e);
-  };
 
   const runOverviews = useMemo(
     () =>
@@ -54,50 +43,41 @@ const RunsAndSubmission = () => {
   const runBoxStyle =
     pipelineRuns.length > 4 ? `h-[165px]` : `h-[${pipelineRuns.length * 50}px]`;
 
-  const runsStyle = runsIsOpen ? "block" : "hidden";
-
   return (
-    <Collapsible
-      className="group/collapsible"
-      onOpenChange={handleRunsCollapsedChange}
-    >
-      <SidebarGroup>
-        <SidebarGroupLabel asChild>
-          <CollapsibleTrigger>
-            Runs & Submissions
-            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-          </CollapsibleTrigger>
-        </SidebarGroupLabel>
-        <CollapsibleContent>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <OasisSubmitter
-                  componentSpec={componentSpec}
-                  onSubmitComplete={refetch}
-                />
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </CollapsibleContent>
-        <SidebarGroupContent>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <ScrollArea
-                className={cn(
-                  "bg-gray-100 border rounded-sm",
-                  runsStyle,
-                  runBoxStyle,
-                  runOverviews.length === 0 ? "hidden" : "",
-                )}
-              >
-                <div className="flex flex-col">{runOverviews}</div>
-              </ScrollArea>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroupContent>
-      </SidebarGroup>
-    </Collapsible>
+    <SidebarGroup>
+      <SidebarGroupLabel asChild>
+        <div className="flex items-center">
+          <span className="font-medium text-sm">Runs & Submissions</span>
+        </div>
+      </SidebarGroupLabel>
+
+      <SidebarGroupContent>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <OasisSubmitter
+              componentSpec={componentSpec}
+              onSubmitComplete={refetch}
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+
+      <SidebarGroupContent>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <ScrollArea
+              className={cn(
+                "bg-gray-100 border rounded-sm",
+                runBoxStyle,
+                runOverviews.length === 0 ? "hidden" : "",
+              )}
+            >
+              <div className="flex flex-col">{runOverviews}</div>
+            </ScrollArea>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
 
