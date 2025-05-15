1bc1dde1d2ec68f4ff1739e61b56baf767f5f4c3
mbeaulne <matt.beaulne@gmail.com>
2025-05-15 15:40:29 -0400

    If the formatBytes is 0 don't show text

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,5 +1,5 @@
 const formatBytes = (bytes: number) => {
-  if (bytes === 0) return "0 Bytes";
+  if (bytes === 0) return "0";
   const k = 1024;
   const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
