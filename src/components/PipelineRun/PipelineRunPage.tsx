4ce94906d6c24f52deae13ddfc6d199d43374bd5
Matt Beaulne <matt.beaulne@shopify.com>
2025-06-10 21:44:57 -0400

    Adds componentLibraryProvider to runs page (#330)

--- src/components/PipelineRun/PipelineRunPage.tsx
+++ src/components/PipelineRun/PipelineRunPage.tsx
@@ -2,6 +2,7 @@ import { Background, MiniMap, type ReactFlowProps } from "@xyflow/react";
 import { useCallback, useState } from "react";
 
 import { FlowCanvas, FlowControls } from "@/components/shared/ReactFlow";
+import { ComponentLibraryProvider } from "@/providers/ComponentLibraryProvider";
 
 const GRID_SIZE = 10;
 
@@ -26,16 +27,18 @@ const PipelineRunPage = () => {
   );
 
   return (
-    <div className="reactflow-wrapper h-full w-full">
-      <FlowCanvas {...flowConfig} readOnly>
-        <MiniMap position="bottom-left" pannable />
-        <FlowControls
-          style={{ marginLeft: "224px", marginBottom: "24px" }}
-          updateConfig={updateFlowConfig}
-        />
-        <Background gap={GRID_SIZE} className="bg-slate-50!" />
-      </FlowCanvas>
-    </div>
+    <ComponentLibraryProvider>
+      <div className="reactflow-wrapper h-full w-full">
+        <FlowCanvas {...flowConfig} readOnly>
+          <MiniMap position="bottom-left" pannable />
+          <FlowControls
+            style={{ marginLeft: "224px", marginBottom: "24px" }}
+            updateConfig={updateFlowConfig}
+          />
+          <Background gap={GRID_SIZE} className="bg-slate-50!" />
+        </FlowCanvas>
+      </div>
+    </ComponentLibraryProvider>
   );
 };
 
