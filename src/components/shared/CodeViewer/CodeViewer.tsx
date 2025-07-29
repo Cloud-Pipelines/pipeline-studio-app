4d0ab14e785ded60c7bbb28977055c9bbb801d3f
Maksym Yezhov <maxim.ezhov@shopify.com>
2025-07-28 23:27:43 -0700

    Inline fullscreen element

--- src/components/shared/CodeViewer/CodeViewer.tsx
+++ src/components/shared/CodeViewer/CodeViewer.tsx
@@ -1,8 +1,9 @@
-import { Maximize2 } from "lucide-react";
-import { useCallback } from "react";
+import { Maximize2, X as XIcon } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 
+import { FullscreenElement } from "../FullscreenElement";
 import { useBetaFlagValue } from "../Settings/useBetaFlags";
 import CodeSyntaxHighlighter from "./CodeSyntaxHighlighter";
 import { useFullscreen } from "./FullscreenCodeViewer";
@@ -25,16 +26,50 @@ const CodeViewer = ({
   onFullscreenChange = () => {},
 }: CodeViewerProps) => {
   const isVirtualized = useBetaFlagValue("codeViewer");
+  const isFullscreenElement = useBetaFlagValue("fullscreen-code-viewer");
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   const { openFullscreen } = useFullscreen();
 
   const handleEnterFullscreen = useCallback(() => {
-    openFullscreen({ code, language, title });
-    onFullscreenChange(true);
-  }, [code, language, title, openFullscreen, onFullscreenChange]);
-  return (
+    if (isFullscreenElement) {
+      setIsFullscreen((prev) => !prev);
+      onFullscreenChange(!isFullscreen);
+    } else {
+      openFullscreen({ code, language, title });
+      onFullscreenChange(true);
+    }
+  }, [
+    code,
+    language,
+    title,
+    openFullscreen,
+    onFullscreenChange,
+    isFullscreenElement,
+    isFullscreen,
+  ]);
+
+  // Handle Escape key
+  useEffect(() => {
+    const handleEscapeKey = (e: KeyboardEvent) => {
+      if (isFullscreen && e.key === "Escape") {
+        setIsFullscreen(false);
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isFullscreen]);
+
+  const codeViewer = (
     <>
-      <div className="border rounded-md h-full bg-slate-900 flex flex-col">
+      <div className="h-full bg-slate-900 flex flex-col">
         <div className="flex justify-between items-center p-2 sticky top-0 z-10 bg-slate-800">
           <h3 className="text-secondary font-medium ml-2">
             {filename} <span className="text-sm">(Read Only)</span>
@@ -44,9 +79,13 @@ const CodeViewer = ({
             size="icon"
             onClick={handleEnterFullscreen}
             className="text-gray-300 hover:text-slate-800"
-            title="View fullscreen"
+            title={isFullscreen ? "Exit fullscreen" : "View fullscreen"}
           >
-            <Maximize2 className="size-4" />
+            {isFullscreen ? (
+              <XIcon className="size-4" />
+            ) : (
+              <Maximize2 className="size-4" />
+            )}
           </Button>
         </div>
         <div className="flex-1 relative">
@@ -65,6 +104,14 @@ const CodeViewer = ({
         </div>
       </div>
     </>
+  );
+
+  return isFullscreenElement ? (
+    <FullscreenElement fullscreen={isFullscreen}>
+      {codeViewer}
+    </FullscreenElement>
+  ) : (
+    codeViewer
   );
 };
 
---
+++ src/components/shared/FullscreenElement/FullscreenElement.tsx
@@ -0,0 +1,82 @@
+import {
+  type PropsWithChildren,
+  type RefObject,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+import { createPortal } from "react-dom";
+
+import { cn } from "@/lib/utils";
+
+interface FullscreenElementProps extends PropsWithChildren {
+  fullscreen: boolean;
+  defaultMountElement: RefObject<HTMLElement | null>;
+}
+
+function FullscreenElementPortal({
+  children,
+  fullscreen,
+  defaultMountElement,
+}: FullscreenElementProps) {
+  const id = useRef(Math.random().toString(15).substring(2, 15));
+  const containerElementRef = useRef<HTMLElement>(
+    document.createElement("div"),
+  );
+
+  useEffect(() => {
+    containerElementRef.current.dataset.testid = "fullscreen-container";
+    if (fullscreen) {
+      containerElementRef.current.className = cn(
+        "fixed",
+        "top-0",
+        "left-0",
+        "z-[2147483647]",
+        "w-full",
+        "h-full",
+        "overflow-hidden",
+      );
+      document.body.appendChild(containerElementRef.current);
+    } else {
+      containerElementRef.current.className = cn(
+        "contents",
+        "pointer-events-auto",
+      );
+
+      if (defaultMountElement.current) {
+        defaultMountElement.current.appendChild(containerElementRef.current);
+      }
+    }
+
+    return () => {
+      containerElementRef.current.remove();
+    };
+  }, [fullscreen, defaultMountElement]);
+
+  const fragment = useMemo(() => <>{children}</>, [children]);
+
+  return createPortal(fragment, containerElementRef.current, id.current);
+}
+
+export function FullscreenElement({
+  children,
+  fullscreen,
+}: PropsWithChildren<{ fullscreen: boolean }>) {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <div
+        ref={mountRef}
+        data-testid="fullscreen-element-mounting-point"
+        style={{ display: "contents" }}
+      />
+      <FullscreenElementPortal
+        fullscreen={fullscreen}
+        defaultMountElement={mountRef}
+      >
+        {children}
+      </FullscreenElementPortal>
+    </>
+  );
+}
