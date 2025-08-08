c32c597f43b193e532ce298be425958c7821206b
Maksym Yezhov <maxim.ezhov@shopify.com>
2025-08-08 10:57:04 -0700

    Playground - basic layout primitives

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,10 +1,25 @@
+import { cva } from "class-variance-authority";
+
 import { cn } from "@/lib/utils";
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+const skeletonVariants = cva("bg-accent animate-pulse rounded-md", {
+  variants: {
+    size: {
+      sm: "h-4 w-24",
+      lg: "h-4 w-32",
+    },
+  },
+});
+
+function Skeleton({
+  className,
+  size = "sm",
+  ...props
+}: React.ComponentProps<"div"> & { size?: "sm" | "lg" }) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn(skeletonVariants({ size }), className)}
       {...props}
     />
   );
