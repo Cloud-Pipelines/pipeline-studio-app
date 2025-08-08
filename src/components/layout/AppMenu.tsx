5341cfdc243d28c5ea61bd15da1f48b5f71472bc
mbeaulne <matt.beaulne@gmail.com>
2025-08-08 14:48:59 -0400

    Moves component spec provider

--- src/components/layout/AppMenu.tsx
+++ src/components/layout/AppMenu.tsx
@@ -1,12 +1,15 @@
 import { Link } from "@tanstack/react-router";
 
 import ImportPipeline from "@/components/shared/ImportPipeline";
+import { useComponentSpec } from "@/providers/ComponentSpecProvider";
 import { TOP_NAV_HEIGHT } from "@/utils/constants";
 
 import BackendStatus from "../shared/BackendStatus";
 import NewPipelineButton from "../shared/NewPipelineButton";
 
 const AppMenu = () => {
+  const { componentSpec } = useComponentSpec();
+  const title = componentSpec?.name;
   return (
     <div
       className="w-full bg-stone-900 p-2"
@@ -21,6 +24,7 @@ const AppMenu = () => {
               className="w-10 h-10 filter invert cursor-pointer"
             />
           </Link>
+          <span className="text-white text-sm font-bold">{title}</span>
         </div>
         <div className="flex flex-row gap-32 items-center">
           <div className="flex flex-row gap-2 items-center">
