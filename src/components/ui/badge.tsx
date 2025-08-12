a172ea515666b251a90e73146b96c14f482064a0
Maksym Yezhov <maxim.ezhov@shopify.com>
2025-08-11 22:03:55 -0700

    improve ui primitives

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,9 +8,18 @@ const badgeVariants = cva(
   "inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
+      size: {
+        xs: "text-xs",
+        md: "text-sm",
+      },
+      display: {
+        inline: "inline-flex",
+        block: "block",
+        absolute: "absolute -top-1 -right-1",
+      },
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+        default: "border-transparent bg-primary text-primary-foreground",
+        inform: "p-0.5 w-4 h-4 bg-primary text-primary-foreground",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
         destructive:
@@ -21,6 +30,8 @@ const badgeVariants = cva(
     },
     defaultVariants: {
       variant: "default",
+      display: "inline",
+      size: "md",
     },
   },
 );
@@ -28,6 +39,8 @@ const badgeVariants = cva(
 function Badge({
   className,
   variant,
+  display,
+  size,
   asChild = false,
   ...props
 }: React.ComponentProps<"span"> &
@@ -37,7 +50,7 @@ function Badge({
   return (
     <Comp
       data-slot="badge"
-      className={cn(badgeVariants({ variant }), className)}
+      className={cn(badgeVariants({ variant, display, size }), className)}
       {...props}
     />
   );
