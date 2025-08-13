97454767f74ca5e6800da712db4fad7efc9ddefe
Maksym Yezhov <maxim.ezhov@shopify.com>
2025-08-13 13:40:09 -0700

    improve ui primitives

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -7,6 +7,13 @@ const skeletonVariants = cva("bg-accent animate-pulse rounded-md", {
     size: {
       sm: "h-4 w-24",
       lg: "h-4 w-32",
+      full: "h-4 w-full",
+      half: "h-4 w-1/2",
+    },
+    shape: {
+      square: "rounded-md",
+      circle: "rounded-full",
+      button: "rounded-md h-8",
     },
   },
 });
@@ -14,12 +21,16 @@ const skeletonVariants = cva("bg-accent animate-pulse rounded-md", {
 function Skeleton({
   className,
   size = "sm",
+  shape = "square",
   ...props
-}: React.ComponentProps<"div"> & { size?: "sm" | "lg" }) {
+}: React.ComponentProps<"div"> & {
+  size?: "sm" | "lg" | "full" | "half";
+  shape?: "square" | "circle" | "button";
+}) {
   return (
     <div
       data-slot="skeleton"
-      className={cn(skeletonVariants({ size }), className)}
+      className={cn(skeletonVariants({ size, shape }), className)}
       {...props}
     />
   );
