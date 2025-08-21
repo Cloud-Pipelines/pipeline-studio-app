04bed061a0ab48278f96927f9446b7f8a6d7136c
Maksym Yezhov <maxim.ezhov@shopify.com>
2025-08-20 23:29:45 -0700

    global alert for outdated components

--- src/components/shared/ReactFlow/FlowSidebar/sections/GraphComponents.tsx
+++ src/components/shared/ReactFlow/FlowSidebar/sections/GraphComponents.tsx
@@ -1,15 +1,11 @@
-import {
-  Cable,
-  Folder,
-  LayoutGrid,
-  PackagePlus,
-  Puzzle,
-  Star,
-} from "lucide-react";
+import { PackagePlus } from "lucide-react";
 import { type ChangeEvent, useCallback, useMemo } from "react";
 
+import { InfoBox } from "@/components/shared/InfoBox";
+import { useOutdatedComponents } from "@/components/shared/ManageComponent/hooks/useOutdatedComponents";
 import { useBetaFlagValue } from "@/components/shared/Settings/useBetaFlags";
-import { BlockStack } from "@/components/ui/layout";
+import { Button } from "@/components/ui/button";
+import { BlockStack, InlineStack } from "@/components/ui/layout";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarGroup,
@@ -22,10 +18,13 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Paragraph } from "@/components/ui/typography";
 import { useComponentLibrary } from "@/providers/ComponentLibraryProvider";
 import { useForcedSearchContext } from "@/providers/ComponentLibraryProvider/ForcedSearchProvider";
 import type { UIComponentFolder } from "@/types/componentLibrary";
+import type { HydratedComponentReference } from "@/utils/componentSpec";
 
+import { useNodesOverlay } from "../../NodesOverlay/NodesOverlayProvider";
 import {
   EmptyState,
   ErrorState,
@@ -37,6 +36,47 @@ import {
 } from "../components";
 import { IONodeSidebarItem } from "../components/ComponentItem";
 import PublishedComponentsSearch from "../components/PublishedComponentsSearch";
+
+function useUpgradeAllComponentsCallback(
+  outdatedComponents: [
+    HydratedComponentReference,
+    HydratedComponentReference,
+  ][],
+) {
+  const { notifyNode, getNodeIdsByDigest, fitNodeIntoView } = useNodesOverlay();
+
+  return useCallback(async () => {
+    if (outdatedComponents.length === 0) {
+      return;
+    }
+
+    const nodeIds = outdatedComponents.flatMap(([outdated, _]) =>
+      getNodeIdsByDigest(outdated.digest),
+    );
+
+    if (nodeIds.length === 0) {
+      return;
+    }
+
+    const nodeId = nodeIds.pop();
+
+    if (!nodeId) {
+      return;
+    }
+
+    await fitNodeIntoView(nodeId);
+
+    notifyNode(nodeId, {
+      type: "update-overlay",
+      data: {
+        replaceWith: new Map(
+          outdatedComponents.map(([outdated, mrc]) => [outdated.digest, mrc]),
+        ),
+        ids: nodeIds,
+      },
+    });
+  }, [getNodeIdsByDigest, fitNodeIntoView, notifyNode, outdatedComponents]);
+}
 
 const GraphComponents = ({ isOpen }: { isOpen: boolean }) => {
   const remoteComponentLibrarySearchEnabled = useBetaFlagValue(
@@ -54,6 +94,10 @@ const GraphComponents = ({ isOpen }: { isOpen: boolean }) => {
     searchResult,
   } = useComponentLibrary();
 
+  const { data: outdatedComponents } = useOutdatedComponents(
+    usedComponentsFolder?.components ?? [],
+  );
+
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     updateSearchFilter({
       searchTerm: e.target.value,
@@ -68,6 +112,9 @@ const GraphComponents = ({ isOpen }: { isOpen: boolean }) => {
     },
     [updateSearchFilter],
   );
+
+  const upgradeAllComponentsCallback =
+    useUpgradeAllComponentsCallback(outdatedComponents);
 
   const memoizedContent = useMemo(() => {
     if (isLoading) return <LoadingState />;
@@ -96,56 +143,83 @@ const GraphComponents = ({ isOpen }: { isOpen: boolean }) => {
       userComponentsFolder?.components &&
       userComponentsFolder.components.length > 0;
 
+    const showOutdatedComponentsAlert =
+      remoteComponentLibrarySearchEnabled && outdatedComponents.length > 0;
+
     return (
-      <BlockStack>
-        {hasUsedComponents && (
+      <BlockStack gap="2">
+        {showOutdatedComponentsAlert ? (
+          <BlockStack className="px-2">
+            <InfoBox title="Upgrades available" key="outdated-components">
+              <BlockStack gap="2">
+                <Paragraph size="xs">
+                  You have {outdatedComponents.length} outdated components used
+                  in your Pipeline.
+                </Paragraph>
+                <InlineStack align="space-between" className="w-full">
+                  <Button size="xs" variant="secondary">
+                    Dismiss
+                  </Button>
+                  <Button size="xs" onClick={upgradeAllComponentsCallback}>
+                    Review
+                  </Button>
+                </InlineStack>
+              </BlockStack>
+            </InfoBox>
+          </BlockStack>
+        ) : null}
+
+        <BlockStack>
+          {hasUsedComponents && (
+            <FolderItem
+              key="used-components-folder"
+              folder={usedComponentsFolder}
+              icon="LayoutGrid"
+              hasAlertBadge={showOutdatedComponentsAlert}
+            />
+          )}
+          {hasFavouriteComponents && (
+            <FolderItem
+              key="favorite-components-folder"
+              folder={favoritesFolder}
+              icon="Star"
+            />
+          )}
+          {hasUserComponents && (
+            <FolderItem
+              key="my-components-folder"
+              folder={userComponentsFolder}
+              icon="Puzzle"
+            />
+          )}
+          <Separator />
           <FolderItem
-            key="used-components-folder"
-            folder={usedComponentsFolder}
-            icon={LayoutGrid}
+            key="graph-inputs-outputs-folder"
+            folder={
+              {
+                name: "Inputs & Outputs",
+                components: [
+                  <IONodeSidebarItem key="input" nodeType="input" />,
+                  <IONodeSidebarItem key="output" nodeType="output" />,
+                ],
+                folders: [],
+              } as UIComponentFolder
+            }
+            icon="Cable"
           />
-        )}
-        {hasFavouriteComponents && (
+          <Separator />
           <FolderItem
-            key="favorite-components-folder"
-            folder={favoritesFolder}
-            icon={Star}
+            key="standard-library-folder"
+            folder={
+              {
+                name: "Standard library",
+                components: [],
+                folders: componentLibrary.folders,
+              } as UIComponentFolder
+            }
+            icon="Folder"
           />
-        )}
-        {hasUserComponents && (
-          <FolderItem
-            key="my-components-folder"
-            folder={userComponentsFolder}
-            icon={Puzzle}
-          />
-        )}
-        <Separator />
-        <FolderItem
-          key="graph-inputs-outputs-folder"
-          folder={
-            {
-              name: "Inputs & Outputs",
-              components: [
-                <IONodeSidebarItem key="input" nodeType="input" />,
-                <IONodeSidebarItem key="output" nodeType="output" />,
-              ],
-              folders: [],
-            } as UIComponentFolder
-          }
-          icon={Cable}
-        />
-        <Separator />
-        <FolderItem
-          key="standard-library-folder"
-          folder={
-            {
-              name: "Standard library",
-              components: [],
-              folders: componentLibrary.folders,
-            } as UIComponentFolder
-          }
-          icon={Folder}
-        />
+        </BlockStack>
       </BlockStack>
     );
   }, [
