38e11bfa2df5761892c8848947e09d1d94791eef
Camiel vs <93798047+camielvs@users.noreply.github.com>
2025-08-26 09:03:14 -0700

    CreateInputNode and CreateOutputNode Methods (#694)
    ## Description
    
    <!-- Please provide a brief description of the changes made in this pull request. Include any relevant context or reasoning for the changes. -->
    Moves IO node creation into separate methods so they can be reused by IO duplication actions (similar to task nodes).
    
    No change to app functionality; just setting up for IO node duplication functionality.
    
    ## Related Issue and Pull requests
    
    <!-- Link to any related issues using the format #<issue-number> -->
    Progresses https://github.com/Shopify/oasis-frontend/issues/148
    
    ## Type of Change
    
    <!-- Please delete options that are not relevant -->
    
    - [x] Cleanup/Refactor
    
    ## Checklist
    
    <!-- Please ensure the following are completed before submitting the PR -->
    
    - [ ] I have tested this does not break current pipelines / runs functionality
    - [ ] I have tested the changes on staging
    
    ## Screenshots (if applicable)
    
    <!-- Include any screenshots that might help explain the changes or provide visual context -->
    
    ## Test Instructions
    
    <!-- Detail steps and prerequisites for testing the changes in this PR -->
    1. confirm that IO nodes work exactly the same as before
    
    ## Additional Comments
    
    <!-- Add any additional context or information that reviewers might need to know regarding this PR -->

---
+++ src/utils/nodes/createInputNode.ts
@@ -0,0 +1,22 @@
+import { type Node } from "@xyflow/react";
+
+import type { InputSpec } from "../componentSpec";
+import { extractPositionFromAnnotations } from "./extractPositionFromAnnotations";
+import { inputNameToNodeId } from "./nodeIdUtils";
+
+export const createInputNode = (input: InputSpec) => {
+  const { name, annotations, ...rest } = input;
+
+  const position = extractPositionFromAnnotations(annotations);
+  const nodeId = inputNameToNodeId(name);
+
+  return {
+    id: nodeId,
+    data: {
+      ...rest,
+      label: name,
+    },
+    position: position,
+    type: "input",
+  } as Node;
+};
