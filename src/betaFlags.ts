347361e11ffd91f168348e04058696ef9ae0330e
Camiel vs <93798047+camielvs@users.noreply.github.com>
2025-09-04 11:29:26 -0700

    Add Config Option for Default CreatedByMe Filter (#796)
    ## Description
    
    <!-- Please provide a brief description of the changes made in this pull request. Include any relevant context or reasoning for the changes. -->
    
    Adds a betaflag that sets the default state of the "Create by me" filter to true or false. i.e. allows users to have their run list filtered to just themselves by default.
    
    ## Related Issue and Pull requests
    
    <!-- Link to any related issues using the format #<issue-number> -->
    
    User-requested
    
    ## Type of Change
    
    <!-- Please delete options that are not relevant -->
    
    - [ ] Bug fix
    - [ ] New feature
    - [x] Improvement
    - [ ] Cleanup/Refactor
    - [ ] Breaking change
    - [ ] Documentation update
    
    ## Checklist
    
    <!-- Please ensure the following are completed before submitting the PR -->
    
    - [ ] I have tested this does not break current pipelines / runs functionality
    - [ ] I have tested the changes on staging
    
    ## Screenshots (if applicable)
    
    <!-- Include any screenshots that might help explain the changes or provide visual context -->
    <img width="1684" height="1008" alt="image" src="https://github.com/user-attachments/assets/fa8c4b9d-1dee-4cee-81c8-c5448a525188" />
    
    
    ## Test Instructions
    
    <!-- Detail steps and prerequisites for testing the changes in this PR -->
    1. activate beta flag in the personal preferences dialog
    2. navigate to the runs list - it should default filter to created by me
    3. turn off the beta flag and go back to the runs list - the filter should no longer be activate by default
    
    ## Additional Comments
    
    <!-- Add any additional context or information that reviewers might need to know regarding this PR -->

--- src/betaFlags.ts
+++ src/betaFlags.ts
@@ -7,6 +7,7 @@ export const ExistingBetaFlags = {
       "Highlight the tasks on the Pipeline canvas when the component is hovered over in the component library.",
     default: false,
   } as BetaFlag,
+
   ["remote-component-library-search"]: {
     name: "Remote component library search",
     description: "Enable the remote component library search.",
@@ -17,6 +18,13 @@ export const ExistingBetaFlags = {
     name: "Redirect on new pipeline run",
     description:
       "Automatically redirect from the editor to the pipeline run page after starting a new execution.",
+    default: false,
+  } as BetaFlag,
+
+  ["created-by-me-default"]: {
+    name: "Default created by me filter",
+    description:
+      "Automatically select the 'Created by me' filter when viewing the pipeline run list.",
     default: false,
   } as BetaFlag,
 };
