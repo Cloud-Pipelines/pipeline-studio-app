258a2cc17fedeb76767ab2dcfef9a409dea64559
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-09-12 11:36:09 -0700

    Cleanup PipelineNameDialog

--- src/components/shared/Dialogs/PipelineNameDialog.tsx
+++ src/components/shared/Dialogs/PipelineNameDialog.tsx
@@ -1,5 +1,4 @@
-import { AlertCircle, InfoIcon } from "lucide-react";
-import { type ChangeEvent, type ReactNode, useState } from "react";
+import { type ChangeEvent, type ReactNode, useCallback, useState } from "react";
 
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
@@ -13,7 +12,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Icon } from "@/components/ui/icon";
 import { Input } from "@/components/ui/input";
+import { BlockStack } from "@/components/ui/layout";
 import useLoadUserPipelines from "@/hooks/useLoadUserPipelines";
 import { VALID_NAME_MESSAGE, VALID_NAME_REGEX } from "@/utils/constants";
 
@@ -22,9 +23,9 @@ interface PipelineNameDialogProps {
   title: string;
   description?: string;
   initialName: string;
-  onSubmit: (name: string) => void;
   submitButtonText: string;
   submitButtonIcon?: ReactNode;
+  onSubmit: (name: string) => void;
   isSubmitDisabled?: (name: string, error: string | null) => boolean;
   onOpenChange?: (open: boolean) => void;
 }
@@ -34,56 +35,63 @@ const PipelineNameDialog = ({
   title,
   description = "Please, name your pipeline.",
   initialName,
-  onSubmit,
   submitButtonText,
   submitButtonIcon,
+  onSubmit,
   isSubmitDisabled,
   onOpenChange,
 }: PipelineNameDialogProps) => {
   const [error, setError] = useState<string | null>(null);
   const [name, setName] = useState(initialName);
+
   const {
     userPipelines,
     isLoadingUserPipelines,
     refetch: refetchUserPipelines,
   } = useLoadUserPipelines();
 
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newName = e.target.value;
-    const existingPipelineNames = new Set(
-      Array.from(userPipelines.keys()).map((name) => name.toLowerCase()),
-    );
+  const handleOnChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const newName = e.target.value;
+      const existingPipelineNames = new Set(
+        Array.from(userPipelines.keys()).map((name) => name.toLowerCase()),
+      );
 
-    if (!VALID_NAME_REGEX.test(newName)) {
-      setError(VALID_NAME_MESSAGE);
-    } else if (existingPipelineNames.has(newName.trim().toLowerCase())) {
-      setError("Name already exists");
-    } else {
-      setError(null);
-    }
+      if (!VALID_NAME_REGEX.test(newName)) {
+        setError(VALID_NAME_MESSAGE);
+      } else if (existingPipelineNames.has(newName.trim().toLowerCase())) {
+        setError("Name already exists");
+      } else {
+        setError(null);
+      }
 
-    setName(e.target.value);
-  };
+      setName(newName);
+    },
+    [userPipelines],
+  );
 
-  const handleDialogOpenChange = (open: boolean) => {
-    if (!open) {
-      setError(null);
-    } else {
-      setName(initialName);
-      refetchUserPipelines();
-    }
-    onOpenChange?.(open);
-  };
+  const handleDialogOpenChange = useCallback(
+    (open: boolean) => {
+      if (!open) {
+        setError(null);
+      } else {
+        setName(initialName);
+        refetchUserPipelines();
+      }
+      onOpenChange?.(open);
+    },
+    [initialName, onOpenChange, refetchUserPipelines],
+  );
 
-  const handleSubmit = () => {
+  const handleSubmit = useCallback(() => {
     onSubmit(name);
-  };
+  }, [name, onSubmit]);
 
   const isDisabled =
     isLoadingUserPipelines ||
     !!error ||
     !name ||
-    (isSubmitDisabled ? isSubmitDisabled(name, error) : false);
+    !!isSubmitDisabled?.(name, error);
 
   return (
     <Dialog onOpenChange={handleDialogOpenChange}>
@@ -93,19 +101,22 @@ const PipelineNameDialog = ({
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
-        <div className="flex items-center space-x-2">
-          <div className="grid flex-1 gap-2 flex-col">
-            <Input value={name} onChange={handleOnChange} />
-            <Alert variant={error ? "destructive" : "default"}>
-              {error && <AlertCircle className="h-4 w-4" />}
-              {!error && <InfoIcon className="h-4 w-4" />}
-              {error && <AlertDescription>{error}</AlertDescription>}
-              {!error && (
+        <BlockStack gap="2">
+          <Input value={name} onChange={handleOnChange} />
+          <Alert variant={error ? "destructive" : "default"}>
+            {error ? (
+              <>
+                <Icon name="CircleAlert" />
+                <AlertDescription>{error}</AlertDescription>
+              </>
+            ) : (
+              <>
+                <Icon name="Info" />
                 <AlertDescription>{VALID_NAME_MESSAGE}</AlertDescription>
-              )}
-            </Alert>
-          </div>
-        </div>
+              </>
+            )}
+          </Alert>
+        </BlockStack>
         <DialogFooter className="sm:justify-end">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
