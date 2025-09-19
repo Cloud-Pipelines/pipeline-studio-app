87e51db889d768d8afe8e666d0ab7f8f586788ea
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-09-19 12:00:15 -0700

    Refactor TaskNode Callback Generation

--- src/components/shared/ReactFlow/FlowCanvas/FlowCanvas.tsx
+++ src/components/shared/ReactFlow/FlowCanvas/FlowCanvas.tsx
@@ -340,13 +340,7 @@ const FlowCanvas = ({
 
       setComponentSpec(updatedComponentSpec);
     },
-    [
-      reactFlowInstance,
-      componentSpec,
-      nodeData,
-      setComponentSpec,
-      updateOrAddNodes,
-    ],
+    [reactFlowInstance, componentSpec, setComponentSpec, updateOrAddNodes],
   );
 
   useEffect(() => {
