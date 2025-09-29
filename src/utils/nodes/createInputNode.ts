3ee440bfebf94d953ffa759cbdf73cdbee7de70b
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-09-29 13:27:07 -0700

    Implement Node Manager

--- src/utils/nodes/createInputNode.ts
+++ src/utils/nodes/createInputNode.ts
@@ -3,19 +3,16 @@ import { type Node } from "@xyflow/react";
 import type { IONodeData, NodeData } from "@/types/nodes";
 
 import type { InputSpec } from "../componentSpec";
-import { inputIdToNodeId, inputNameToInputId } from "./conversions";
 import { extractPositionFromAnnotations } from "./extractPositionFromAnnotations";
 
 export const createInputNode = (input: InputSpec, nodeData: NodeData) => {
   const { name, annotations } = input;
   const { nodeManager, readOnly } = nodeData;
 
-  const newNodeId = nodeManager?.getNodeId(name, "input");
-  console.log("Creating input node:", { name, nodeId: newNodeId });
+  const nodeId = nodeManager?.getNodeId(name, "input");
+  console.log("Creating input node:", { name, nodeId });
 
   const position = extractPositionFromAnnotations(annotations);
-  const inputId = inputNameToInputId(name);
-  const nodeId = inputIdToNodeId(inputId);
 
   const inputNodeData: IONodeData = {
     spec: input,
--- src/utils/nodes/createOutputNode.ts
+++ src/utils/nodes/createOutputNode.ts
@@ -3,19 +3,16 @@ import { type Node } from "@xyflow/react";
 import type { IONodeData, NodeData } from "@/types/nodes";
 
 import type { OutputSpec } from "../componentSpec";
-import { outputIdToNodeId, outputNameToOutputId } from "./conversions";
 import { extractPositionFromAnnotations } from "./extractPositionFromAnnotations";
 
 export const createOutputNode = (output: OutputSpec, nodeData: NodeData) => {
   const { name, annotations } = output;
   const { nodeManager, readOnly } = nodeData;
 
-  const newNodeId = nodeManager?.getNodeId(name, "output");
-  console.log("Creating output node:", { name, nodeId: newNodeId });
+  const nodeId = nodeManager?.getNodeId(name, "output");
+  console.log("Creating output node:", { name, nodeId });
 
   const position = extractPositionFromAnnotations(annotations);
-  const outputId = outputNameToOutputId(name);
-  const nodeId = outputIdToNodeId(outputId);
 
   const outputNodeData: IONodeData = {
     spec: output,
