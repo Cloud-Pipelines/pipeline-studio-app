50cbbc293473e41ff05250a418c0db658d7e4162
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-09-29 13:27:07 -0700

    Implement Node Manager

--- src/utils/nodes/createInputNode.ts
+++ src/utils/nodes/createInputNode.ts
@@ -4,17 +4,15 @@ import type { IONodeData, NodeData } from "@/types/nodes";
 
 import type { InputSpec } from "../componentSpec";
 import { extractPositionFromAnnotations } from "./extractPositionFromAnnotations";
-import { inputNameToNodeId } from "./nodeIdUtils";
 
 export const createInputNode = (input: InputSpec, nodeData: NodeData) => {
   const { name, annotations } = input;
   const { nodeManager, readOnly } = nodeData;
 
-  const newNodeId = nodeManager?.getNodeId(name, "input");
-  console.log("Creating input node:", { name, nodeId: newNodeId });
+  const nodeId = nodeManager?.getNodeId(name, "input");
+  console.log("Creating input node:", { name, nodeId });
 
   const position = extractPositionFromAnnotations(annotations);
-  const nodeId = inputNameToNodeId(name);
 
   const inputNodeData: IONodeData = {
     spec: input,
