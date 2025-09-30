16348eaf609d537b86670a1b52167f77ff526d2e
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2025-09-29 17:18:44 -0700

    Add debug mode for viewing ReactFlow Node ids

--- src/components/shared/ReactFlow/FlowCanvas/IONode/IONode.tsx
+++ src/components/shared/ReactFlow/FlowCanvas/IONode/IONode.tsx
@@ -1,5 +1,5 @@
 import { Handle, Position } from "@xyflow/react";
-import { memo, useEffect, useMemo } from "react";
+import { memo, useCallback, useEffect, useMemo } from "react";
 
 import { InputValueEditor } from "@/components/Editor/IOEditor/InputValueEditor";
 import { OutputNameEditor } from "@/components/Editor/IOEditor/OutputNameEditor";
@@ -12,6 +12,13 @@ import { useComponentSpec } from "@/providers/ComponentSpecProvider";
 import { useContextPanel } from "@/providers/ContextPanelProvider";
 import type { IONodeData } from "@/types/nodes";
 import type { InputSpec, TypeSpecType } from "@/utils/componentSpec";
+import { ENABLE_DEBUG_MODE } from "@/utils/constants";
+import {
+  inputIdToNodeId,
+  inputNameToInputId,
+  outputIdToNodeId,
+  outputNameToOutputId,
+} from "@/utils/nodes/conversions";
 
 interface IONodeProps {
   type: "input" | "output";
@@ -50,6 +57,22 @@ const IONode = ({ type, data, selected = false }: IONodeProps) => {
     () => componentSpec.outputs?.find((output) => output.name === spec.name),
     [componentSpec.outputs, spec.name],
   );
+
+  const nodeId = isInput
+    ? inputIdToNodeId(inputNameToInputId(spec.name))
+    : outputIdToNodeId(outputNameToOutputId(spec.name));
+
+  const nodeHandleId = `${nodeId}_handle`;
+
+  const handleHandleClick = useCallback(() => {
+    if (ENABLE_DEBUG_MODE) {
+      console.log(`${isInput ? "Input" : "Output"} Node Handle clicked:`, {
+        name: isInput ? input?.name : output?.name,
+        nodeId,
+        handleId: nodeHandleId,
+      });
+    }
+  }, [isInput, input, output, nodeId, nodeHandleId]);
 
   useEffect(() => {
     if (selected) {
@@ -112,6 +135,11 @@ const IONode = ({ type, data, selected = false }: IONodeProps) => {
     <Card className={cn("border-2 max-w-[300px] p-0", borderColor)}>
       <CardHeader className="px-2 py-2.5">
         <CardTitle className="break-words">{spec.name}</CardTitle>
+        {ENABLE_DEBUG_MODE && (
+          <Paragraph size="xs" tone="subdued">
+            Node Id: {nodeId}
+          </Paragraph>
+        )}
       </CardHeader>
       <CardContent className="p-2 max-w-[250px]">
         <BlockStack gap="2">
@@ -150,6 +178,7 @@ const IONode = ({ type, data, selected = false }: IONodeProps) => {
           type={handleType}
           position={handlePosition}
           className={cn(handleDefaultClassName, handleClassName)}
+          onClick={handleHandleClick}
         />
       </CardContent>
     </Card>
