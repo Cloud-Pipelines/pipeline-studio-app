dc6948697ebd9c6bc8092cf0e926e776365f1287
mbeaulne <matt.beaulne@gmail.com>
2025-10-16 16:56:54 -0400

    move useCurrentLevelExecutionData deps into function

--- src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskConfiguration/TaskConfiguration.tsx
+++ src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskConfiguration/TaskConfiguration.tsx
@@ -1,4 +1,3 @@
-import { useMatch } from "@tanstack/react-router";
 import {
   AmphoraIcon,
   FilePenLineIcon,
@@ -22,7 +21,6 @@ import {
 } from "@/components/ui/tooltip";
 import { useCurrentLevelExecutionData } from "@/hooks/useCurrentLevelExecutionData";
 import { type TaskNodeContextType } from "@/providers/TaskNodeProvider";
-import { runDetailRoute } from "@/routes/router";
 
 import { AnnotationsSection } from "../AnnotationsEditor/AnnotationsSection";
 import ArgumentsSection from "../ArgumentsEditor/ArgumentsSection";
@@ -41,10 +39,7 @@ interface TaskConfigurationProps {
 const TaskConfiguration = ({ taskNode, actions }: TaskConfigurationProps) => {
   const { name, taskSpec, taskId, state, callbacks } = taskNode;
 
-  const runMatch = useMatch({ from: runDetailRoute.id, shouldThrow: false });
-  const runId = (runMatch?.params as { id?: string })?.id || "";
-
-  const { details } = useCurrentLevelExecutionData(runId);
+  const { details } = useCurrentLevelExecutionData();
 
   const { readOnly, runStatus } = state;
   const disabled = !!runStatus;
