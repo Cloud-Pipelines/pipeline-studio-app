TangleML/tangle-ui

feat: run comparison - artifact comparison

src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskOverview/IOSection/IOCell/ArtifactVisualizer/JsonVisualizer.tsx
@@ -4,22 +4,30 @@ import { useArtifactFetch } from "./useArtifactFetch";
 interface JsonVisualizerValueProps {
   name: string;
   value: string;
+  isFullscreen?: boolean;
 }
 
 interface JsonVisualizerRemoteProps {
   name: string;
   signedUrl: string;
+  isFullscreen?: boolean;
 }
 
 export const JsonVisualizerValue = ({
   name,
   value,
-}: JsonVisualizerValueProps) => <IOCodeViewer title={name} value={value} />;
+  isFullscreen,
+}: JsonVisualizerValueProps) => (
+  <IOCodeViewer title={name} value={value} isFullscreen={isFullscreen} />
+);
 
 export const JsonVisualizerRemote = ({
   name,
   signedUrl,
+  isFullscreen,
 }: JsonVisualizerRemoteProps) => {
   const content = useArtifactFetch("json", signedUrl, (r) => r.text());
-  return <IOCodeViewer title={name} value={content} />;
+  return (
+    <IOCodeViewer title={name} value={content} isFullscreen={isFullscreen} />
+  );
 };

src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskOverview/IOSection/IOCell/IOCodeViewer.tsx
@@ -1,4 +1,5 @@
 import { CodeViewer } from "@/components/shared/CodeViewer";
+import { cn } from "@/lib/utils";
 import { safeJsonParse } from "@/utils/string";
 
 const MAX_LINES = 10;
@@ -8,14 +9,21 @@ const HEADER_HEIGHT = 55;
 interface IOCodeViewerProps {
   title: string;
   value: string;
+  /** Fill the available space instead of capping to a preview height. */
+  isFullscreen?: boolean;
 }
 
-const IOCodeViewer = ({ title, value }: IOCodeViewerProps) => {
+const IOCodeViewer = ({ title, value, isFullscreen }: IOCodeViewerProps) => {
   const { parsed, isValidJson } = safeJsonParse(value);
 
   if (!isValidJson) {
     return (
-      <pre className="w-full font-mono text-xs whitespace-pre-line wrap-break-word">
+      <pre
+        className={cn(
+          "w-full font-mono text-xs whitespace-pre-line wrap-break-word",
+          isFullscreen && "h-full overflow-auto",
+        )}
+      >
         {value || "No value"}
       </pre>
     );
@@ -28,7 +36,10 @@ const IOCodeViewer = ({ title, value }: IOCodeViewerProps) => {
   const lineHeight = `${maxLines * JSON_CODE_LINE_HEIGHT + HEADER_HEIGHT}px`;
 
   return (
-    <div style={{ height: lineHeight }}>
+    <div
+      className={isFullscreen ? "h-full min-h-0 w-full" : undefined}
+      style={isFullscreen ? undefined : { height: lineHeight }}
+    >
       <CodeViewer code={codeString} language="json" filename={title} />
     </div>
   );

Cloud-Pipelines/pipeline-studio-app

src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskOverview/IOSection/IOCell/ArtifactVisualizer/JsonVisualizer.tsx
@@ -4,22 +4,30 @@ import { useArtifactFetch } from "./useArtifactFetch";
 interface JsonVisualizerValueProps {
   name: string;
   value: string;
+  isFullscreen?: boolean;
 }
 
 interface JsonVisualizerRemoteProps {
   name: string;
   signedUrl: string;
+  isFullscreen?: boolean;
 }
 
 export const JsonVisualizerValue = ({
   name,
   value,
-}: JsonVisualizerValueProps) => <IOCodeViewer title={name} value={value} />;
+  isFullscreen,
+}: JsonVisualizerValueProps) => (
+  <IOCodeViewer title={name} value={value} fillContainer={isFullscreen} />
+);
 
 export const JsonVisualizerRemote = ({
   name,
   signedUrl,
+  isFullscreen,
 }: JsonVisualizerRemoteProps) => {
   const content = useArtifactFetch("json", signedUrl, (r) => r.text());
-  return <IOCodeViewer title={name} value={content} />;
+  return (
+    <IOCodeViewer title={name} value={content} fillContainer={isFullscreen} />
+  );
 };

src/components/shared/ReactFlow/FlowCanvas/TaskNode/TaskOverview/IOSection/IOCell/IOCodeViewer.tsx
@@ -1,4 +1,5 @@
 import { CodeViewer } from "@/components/shared/CodeViewer";
+import { cn } from "@/lib/utils";
 import { safeJsonParse } from "@/utils/string";
 
 const MAX_LINES = 10;
@@ -8,14 +9,20 @@ const HEADER_HEIGHT = 55;
 interface IOCodeViewerProps {
   title: string;
   value: string;
+  fillContainer?: boolean;
 }
 
-const IOCodeViewer = ({ title, value }: IOCodeViewerProps) => {
+const IOCodeViewer = ({ title, value, fillContainer }: IOCodeViewerProps) => {
   const { parsed, isValidJson } = safeJsonParse(value);
 
   if (!isValidJson) {
     return (
-      <pre className="w-full font-mono text-xs whitespace-pre-line wrap-break-word">
+      <pre
+        className={cn(
+          "w-full font-mono text-xs whitespace-pre-line wrap-break-word",
+          fillContainer && "h-full overflow-auto",
+        )}
+      >
         {value || "No value"}
       </pre>
     );
@@ -28,7 +35,10 @@ const IOCodeViewer = ({ title, value }: IOCodeViewerProps) => {
   const lineHeight = `${maxLines * JSON_CODE_LINE_HEIGHT + HEADER_HEIGHT}px`;
 
   return (
-    <div style={{ height: lineHeight }}>
+    <div
+      className={fillContainer ? "h-full min-h-0 w-full" : undefined}
+      style={fillContainer ? undefined : { height: lineHeight }}
+    >
       <CodeViewer code={codeString} language="json" filename={title} />
     </div>
   );