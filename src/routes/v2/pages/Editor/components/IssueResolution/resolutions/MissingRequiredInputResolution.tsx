067fa45df176ed4726ee350e1ceab65884c9b050
Camiel van Schoonhoven <camiel.vanschoonhoven@shopify.com>
2026-08-20 12:18:43 -0700

    fix: Give AI full visibility into subgraphs

--- src/routes/v2/pages/Editor/components/IssueResolution/resolutions/MissingRequiredInputResolution.tsx
+++ src/routes/v2/pages/Editor/components/IssueResolution/resolutions/MissingRequiredInputResolution.tsx
@@ -3,8 +3,8 @@ import { observer } from "mobx-react-lite";
 import { BlockStack } from "@/components/ui/layout";
 import { Text } from "@/components/ui/typography";
 import type { ComponentSpec, ValidationIssue } from "@/models/componentSpec";
+import { findTaskById } from "@/models/componentSpec/queries/findTaskById";
 import { ArgumentRow } from "@/routes/v2/pages/Editor/components/ArgumentRow/ArgumentRow";
-import { findTaskById } from "@/routes/v2/pages/Editor/components/IssueResolution/validationResolution.utils";
 
 import { InfoOnlyResolution } from "./InfoOnlyResolution";
 
